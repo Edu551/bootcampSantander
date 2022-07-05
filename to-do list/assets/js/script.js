339e849cbca26dd877357caed7b77c412ff70ab0
Eduardo Moraes Goncalves <eduardo.m.goncalves@unicuritiba.com.br>
2022-07-05 11:14:49 -0300

    Criação da lista de tarefas

---
+++ to-do list/assets/js/script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("task-form");
+const taskList = document.getElementById("tasks");
+
+form.onsubmit = function (e) {
+    const inputField = document.getElementById("task-input");
+
+    if (inputField.value == "" || !inputField.value.trim()) {
+        alert("Preencha o campo com uma tarefa.");
+    } else {
+        e.preventDefault();
+        addTask(inputField.value);
+        form.reset();
+    }
+};
+
+function addTask(description) {
+    const taskContainer = document.createElement("div");
+    const newTask = document.createElement("input");
+    const taskLabel = document.createElement("label");
+    const taskDescriptionNode = document.createTextNode(description);
+
+    newTask.setAttribute("type", "checkbox");
+    newTask.setAttribute("name", description);
+    newTask.setAttribute("id", description);
+
+    taskLabel.setAttribute("for", description);
+    taskLabel.appendChild(taskDescriptionNode);
+
+    taskContainer.classList.add("task-item");
+    taskContainer.appendChild(newTask);
+    taskContainer.appendChild(taskLabel);
+
+    taskList.appendChild(taskContainer);
+}
